52adf1758c73c7c36951fd59d7287ce2d2acc982
Jonathan Navas <jonathan.navas9@hotmail.com>
2021-09-30 17:12:50 -0500

    Custom Hook useFetchGifs added

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,34 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
+    const { data: images, loading } = useFetchGifs( category );
 
-    useEffect( () => {
-        getGifs();
-    },[])
-
-    const getGifs = async() => {
-        const url =`https://api.giphy.com/v1/gifs/search?q=ecuador&limit=10&api_key=fWo7dXt78TPBtVXQgSvIjWhlji1a5vG8`;
-        const resp = await fetch(url);
-        const {data} = await resp.json();
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        });
-        console.log(gifs);
-
-        setImages(gifs);
-    }
-    // getGifs();
     return (
         <> 
             <h3>{ category }</h3>  
-            <div className="card-grid">
+
+            { loading && <p>Loading</p> }
+        
+            { 
+                <div className="card-grid">
             
                 {
                     images.map( (img) => (
@@ -39,7 +24,8 @@ export const GifGrid = ({ category }) => {
                     ))
                 }
                 
-            </div>
+                </div>
+            }
         </>
     )
 }
